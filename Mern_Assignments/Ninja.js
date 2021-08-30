e5b8c578439491919b1b2c16559352a23e0b2c80
Iskander Rangel <icerangel@outlook.com>
2021-08-30 15:24:24 -0700

    W1D1

---
+++ Mern_Assignments/Ninja.js
@@ -0,0 +1,30 @@
+
+
+class Ninja {
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName(){
+        console.log("This is your name:" + this.name);
+    }
+
+    showStats(){
+        console.log("Name:" + this.name + "\nStrength:" + this.strength);
+        console.log("Speed:" + this.speed + "\nHealth" + this.health);
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
