261a74f923a92ee7aa6e0f210611b0723a1691a2
Iskander Rangel <icerangel@outlook.com>
2021-08-31 08:37:57 -0700

    W1D2

--- Mern_Assignments/Super_Ninja.js
+++ Mern_Assignments/Super_Ninja.js
@@ -1,6 +1,6 @@
 
-
 // Creating a ninja class
+
 class Ninja {
     constructor(name){ // Constructor that will take in a name argument 
         this.name = name; // Setting each attribute in the Ninja calss
@@ -24,11 +24,47 @@ class Ninja {
     }
 
 }
+    // Creating the Sensei class
+class Sensei extends Ninja {
+    constructor(name){
+        super(name); // ADD COMMENTS 
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    // Methods 
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Hay mas tiempo que vida!")
+    }
+
+    sayName() {
+        const childFunc = super.sayName();
+    }
+
+    sake() {
+        const sakeChild = super.drinkSake();
+        console.log(this.health);
+    }
+
+}
+
 
 
 // Instantiating the Ninja class object 
 const ninja1 = new Ninja("Ice"); // We need to pass in an aggument for the Ninja class 
+
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+
+// Instantiating the Sensei class object 
+const sensei1 = new Sensei("Iskander");
+
+sensei1.sayName();
+sensei1.showStats();
+sensei1.speakWisdom();
