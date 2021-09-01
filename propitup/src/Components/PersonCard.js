40d92c802e456e3c4a7ae3adf9388374f7965e1a
Iskander Rangel <icerangel@outlook.com>
2021-09-01 13:17:49 -0700

    W1D3

--- propitup/src/Components/PersonCard.js
+++ propitup/src/Components/PersonCard.js
@@ -4,13 +4,28 @@ import React, {Component} from 'react';
 
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    // Methods 
+
+    increaseAge = () => {
+        this.setState({
+            age: this.state.age + 1
+        })
+    }
 
     render(){
         return (
             <div id = "outline">
                 <p class = "header">{this.props.header}</p>
-                <p> Age: {this.props.age}</p>
+                <p> Age: {this.state.age}</p>
                 <p> Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.increaseAge}>increase {this.props.header}'s age +1</button>
             </div>
         );
     }
